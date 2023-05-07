6003df2a1ed6937b8f3be424a533523c8be7ae33
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-07 03:35:59 +0100

    added a constructor and variable lifeline

--- contracts/4_Mode.sol
+++ contracts/4_Mode.sol
@@ -2,9 +2,17 @@
 pragma solidity 0.8.0;
 
 contract Mode{
+    //This is assigning a value to the variable after declaring it and cannot be declared again this way
     int public value=10;
     string constant user = "David";
-     
+    int public lifeline;
+
+    //Constructor automatically assigns a value to the variable during compilation
+    constructor(){
+        lifeline= 22;
+    }
+
+    //This is another way to change the value of variable {value} by using a function
     function Change(int _value) public{
         value = _value;
     }
