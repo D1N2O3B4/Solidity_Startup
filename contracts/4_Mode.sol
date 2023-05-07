33f291e4c590e1aee620509ec7cca79f50789956
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-07 03:29:50 +0100

    created a constant variable called name

--- contracts/4_Mode.sol
+++ contracts/4_Mode.sol
@@ -3,7 +3,8 @@ pragma solidity 0.8.0;
 
 contract Mode{
     int public value=10;
-
+    string constant user = "David";
+     
     function Change(int _value) public{
         value = _value;
     }
