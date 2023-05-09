840dfbc02e7f25e44fb579803209063da7372766
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-10 00:51:49 +0100

    modified the constructor changed the parameters

--- contracts/4_Mode.sol
+++ contracts/4_Mode.sol
@@ -6,11 +6,13 @@ contract Mode{
     int public value=10;
     string  public  user = "David";
     int public lifeline;
+    address public  wallet;
 
     //Constructor automatically assigns a value to the variable during compilation
     constructor(int _lifeline,string memory _user){
         lifeline= _lifeline;
         user =  _user;
+        wallet = msg.sender;
     }
 
     //This is another way to change the value of variable {value} by using a function
