4db6815d699930cea44ab8788117b7d20024a99f
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-10 23:25:10 +0100

    modified Safe_Math_Bool.sol

--- contracts/Safe_Math_Bool.sol
+++ contracts/Safe_Math_Bool.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.5.0;
 
 contract Tippy{
     uint8 public sand = 255;
     int8 public  land = -10;
 
+
+  //In this situation the value of sand becomes 0 after incrementing because it has overflown it's limit
     function increment()public {
         sand += 1;
     }
+    
 }
