46a3847e24b9d126d475e25e8091eb603180ef1c
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-10 23:29:16 +0100

    added the unchecked function

--- contracts/Safe_Math_Bool.sol
+++ contracts/Safe_Math_Bool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.5.0;
+pragma solidity 0.8.0;
 
 contract Tippy{
     uint8 public sand = 255;
@@ -8,7 +8,7 @@ contract Tippy{
 
   //In this situation the value of sand becomes 0 after incrementing because it has overflown it's limit
     function increment()public {
-        sand += 1;
+       unchecked{sand += 1;}
     }
     
 }
