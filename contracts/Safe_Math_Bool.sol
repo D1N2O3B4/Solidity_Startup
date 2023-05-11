66d3ffe58a92cd6af706f10fa36d2135c622ccb4
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-11 12:11:34 +0100

    added a comment describing the unchecked func

--- contracts/Safe_Math_Bool.sol
+++ contracts/Safe_Math_Bool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 contract Tippy{
     uint8 public sand = 255;
@@ -8,6 +8,7 @@ contract Tippy{
 
   //In this situation the value of sand becomes 0 after incrementing because it has overflown it's limit
     function increment()public {
+      //The  unchecked is used to stop the error which alerts that 255+1 != 0 
        unchecked{sand += 1;}
     }
     
