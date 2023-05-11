6c37e34a15d787fbaad98b7dd8a46a08af2ebefc
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-11 13:02:16 +0100

    added set_bytes func

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -4,12 +4,22 @@ pragma solidity ^0.8.0;
 contract Arrays{
     uint[5] public grades = [3,56,45];   
     uint[3] public age;
+    bytes1 public  x;
+    bytes5 public  y;
 
     //The function's parameters have to be of type unit
     function set_index_value(uint _value,uint index) public {
         age[index] =  _value;
     }
 
+    //View is used when we do not modify the blockchain
+    function array_lenght() public view returns(uint){
+        return  grades.length;
+    } 
+    function set_bytes() public {
+        x = "a";
+        y = "green";
+    }
 
 }
 
