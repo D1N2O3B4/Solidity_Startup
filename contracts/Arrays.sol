fb3e3f03dc91f228933651a7d1d3550022d1ece5
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-11 12:31:46 +0100

    added a comment for the set_index_value func

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -5,6 +5,7 @@ contract Arrays{
     uint[5] public grades = [3,56,45];   
     uint[3] public age;
 
+    //The function's parameters have to be of type unit
     function set_index_value(uint _value,uint index) public {
         age[index] =  _value;
     }
