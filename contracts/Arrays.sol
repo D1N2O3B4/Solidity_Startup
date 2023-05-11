d1fb098bd3b614509fb245efea3420ebd08cd5a9
d1n2o3b4 <davebostons2017@gmail.com>
2023-05-11 12:29:50 +0100

    created Arrays.sol

---
+++ contracts/Arrays.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;  
+
+contract Arrays{
+    uint[5] public grades = [3,56,45];   
+    uint[3] public age;
+
+    function set_index_value(uint _value,uint index) public {
+        age[index] =  _value;
+    }
+
+
+}
+
